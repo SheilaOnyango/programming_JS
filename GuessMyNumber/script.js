52fe6a462ef5ad14f3d132f82a93e3722f8b2201
sheilaonyango <edusheila31@gmail.com>
2024-04-27 15:37:57 +0300

    Define variables

--- GuessMyNumber/script.js
+++ GuessMyNumber/script.js
@@ -1,2 +1,8 @@
 // Generate a random number between 1 and 20 (inclusive) and assign it to the constant variable secretNumber
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+// Define a variable score with an initial value of 20, representing the player's current score
+let score = 20;
+
+// Define a variable highScore with an initial value of 0 to keep track of the highest score achieved by the player
+let highScore = 0;
