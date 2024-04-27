8d27fecafd901ec8600c8b91471552e82228d258
sheilaonyango <edusheila31@gmail.com>
2024-04-27 14:30:15 +0300

    Select elements

---
+++ Pancake maker 2/script.js
@@ -0,0 +1,8 @@
+// Select elements
+const form = document.querySelector(".form-container");
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+const typeSelect = document.querySelector("#type");
+const orderButton = document.querySelector("#orderButton");
+const totalPriceElement = document.querySelector("#totalPrice");
+const priceBanner = document.querySelector(".price-banner");
+const orderContainer = document.getElementById("order-container");
