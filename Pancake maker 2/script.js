9e2dc3e2632b6560004e9162f810dfe81cfb1d45
sheilaonyango <edusheila31@gmail.com>
2024-04-27 14:31:38 +0300

    define variables

--- Pancake maker 2/script.js
+++ Pancake maker 2/script.js
@@ -6,3 +6,8 @@ const orderButton = document.querySelector("#orderButton");
 const totalPriceElement = document.querySelector("#totalPrice");
 const priceBanner = document.querySelector(".price-banner");
 const orderContainer = document.getElementById("order-container");
+
+// Define variables
+let total = parseInt(typeSelect.value);
+let toppings = [];
+let extras = [];
