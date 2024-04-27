b28dc9f5b15670e7c953d4378bc5b7c6b3e234ee
sheilaonyango <edusheila31@gmail.com>
2024-04-27 15:19:44 +0300

    Select elements

---
+++ Portfolio_Website JS/script.js
@@ -0,0 +1,6 @@
+//Select elements
+const burger = document.querySelector("#burger-menu");
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
+const scrollUp = document.querySelector("#scroll-up");
+const navLink = document.querySelectorAll(".nav-link");
